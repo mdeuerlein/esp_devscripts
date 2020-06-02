c03732883d1583d842c459fcf68b7fbb3b6227a9
Markus Deuerlein <mdeuerlein@users.noreply.github.com>
2020-06-02 11:34:03 +0200

    Update esp8266_4relay.ino

--- sketch/esp8266_4relay/esp8266_4relay.ino
+++ sketch/esp8266_4relay/esp8266_4relay.ino
@@ -1,1 +1,17 @@
-// to be added
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(16, OUTPUT);
+  delay(100);
+
+}
+
+void loop() {
+  digitalWrite(12, HIGH);  //  OFF : Rasensprenger
+  digitalWrite(13, LOW);   //  ON  : Springbrunnen
+  digitalWrite(14, LOW);   //  ON  : Teichumpe
+  digitalWrite(16, LOW);   //. ON  : Buddhafigur
+}
