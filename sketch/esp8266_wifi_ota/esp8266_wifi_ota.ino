023425fe5e7a83236646ce8c6309eb0b8ce3240d
Markus Deuerlein <mdeuerlein@users.noreply.github.com>
2020-06-02 11:20:59 +0200

    Update esp8266_wifi_ota.ino

--- sketch/esp8266_wifi_ota/esp8266_wifi_ota.ino
+++ sketch/esp8266_wifi_ota/esp8266_wifi_ota.ino
@@ -1,1 +1,19 @@
-// to be added
+const char* ssid     = "";
+const char* password = "";
+
+#include "OTA_setup.h"
+
+unsigned long entry;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  setupOTA("ESP8266_4RELAY");
+  delay(100);
+}
+
+void loop() {
+  #ifndef ESP32_RTOS
+  ArduinoOTA.handle();
+  #endif
+}
